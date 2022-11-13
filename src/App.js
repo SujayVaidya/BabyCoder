04f03f3d32ca1d9e73b1cfb0a92718fca4af3171
SujayVaidya <sujayvaidya4@gmail.com>
2022-11-13 18:42:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import { GlobalStyle } from './Styles/Global';
+import Home from './Screens/Home';
+import Playground from './Screens/Playground';
+import Error404 from './Screens/Eror 404';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+   <GlobalStyle/>
+   <Routes>
+      < Route path='/' element = {<Home/> }/>
+      <Route path='/Playground' element = { <Playground/> }/>
+      <Route path='*' element = { <Error404/> } />
+
+
+
+   </Routes>
+   
+   </BrowserRouter>
   );
 }
 
---
+++ src/Screens/Playground/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Playground = () => {
+  return (
+    <div>PlayGround</div>
+  )
+}
+
+export default Playground
